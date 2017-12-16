ba5c7d7f6fd29bdc6295bbbbf96a15a57f9c5fbc
Sourav Bhattacharya <sourav.bhattacharya3@gmail.com>
2017-12-16 12:23:29 +0530

    Add project files.

---
+++ ConsAppRScriptTest/RScriptClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppRScriptTest
+{
+    class RScriptClass
+    {
+        public static string Run()
+        {
+            string result = null;
+
+            string RCodeFilePath = @"C:\Users\Sourav Bhattacharya\Documents\Visual Studio 2017\Projects\ConsAppRScriptTest\ConsAppRScriptTest\Test1.R";
+
+            string RScriptExecutablePath = @"C:\Program Files\R\R-3.4.3\bin\x64\Rscript.exe";
+
+            try
+            {
+                var info = new ProcessStartInfo()
+                {
+                    FileName = RScriptExecutablePath,
+                    WorkingDirectory = Path.GetDirectoryName(RScriptExecutablePath),
+                    Arguments = RCodeFilePath,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var proc = new Process())
+                {
+                    proc.StartInfo = info;
+                    proc.Start();
+                    StreamReader sr = proc.StandardOutput;
+                    result = sr.ReadToEnd();
+                    proc.Close();
+                }
+            }
+            catch (Exception)
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsAppRScriptTest/Test1.R
@@ -0,0 +1,2 @@
+observed <- c(20, 18, 26, 33, 22, 24)
+chisq.test(observed)
